51b61d9c2e819a8d71fe7edd6d593109db5752bc
iobaidu5 <i.obaidu5@gmail.com>
2026-06-18 00:55:47 +0500

    updated

--- assets/ressources/js/bootstrap-datepicker.fr.js
+++ assets/ressources/js/bootstrap-datepicker.fr.js
@@ -1,17 +1,1 @@
-/**
- * French translation for bootstrap-datepicker
- * Nico Mollet <nico.mollet@gmail.com>
- */
-;(function($){
-	$.fn.datepicker.dates['fr'] = {
-		days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
-		daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
-		daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
-		months: ["Janvier", "FÃ©vrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "AoÃ»t", "Septembre", "Octobre", "Novembre", "DÃ©cembre"],
-		monthsShort: ["Jan", "FÃ©v", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "DÃ©c"],
-		today: "Aujourd'hui",
-		clear: "Effacer",
-		weekStart: 1,
-		format: "dd/mm/yyyy"
-	};
-}(jQuery));
+jQuery.fn.datepicker.dates.fr={days:["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"],daysShort:["Dim","Lun","Mar","Mer","Jeu","Ven","Sam","Dim"],daysMin:["D","L","Ma","Me","J","V","S","D"],months:["Janvier","F\xc3\xa9vrier","Mars","Avril","Mai","Juin","Juillet","Ao\xc3\xbbt","Septembre","Octobre","Novembre","D\xc3\xa9cembre"],monthsShort:["Jan","F\xc3\xa9v","Mar","Avr","Mai","Jui","Jul","Aou","Sep","Oct","Nov","D\xc3\xa9c"],today:"Aujourd'hui",clear:"Effacer",weekStart:1,format:"dd/mm/yyyy"};
